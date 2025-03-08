2f04ac04e4af6ffa1820ce4d26d1855eb0b97b34
killer123j <gogogaming7428@gmail.com>
2025-03-08 22:33:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://testofvjfilter-1fa60b1b8498.herokuapp.com/")
+URL = environ.get("URL", "https://thirsty-kellie-killerxvip-5419c084.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
