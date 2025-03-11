f39781758d1177bff1f2b0314717c0a609b8f226
killer123j <gogogaming7428@gmail.com>
2025-03-11 23:57:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,7 +125,7 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'https://inshorturl.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '8f36a769bfcd1db81d4c28e78d5edde74cef7d7d')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/c/2062577653/704') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
@@ -150,9 +150,9 @@ QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 YEARS = ["1900", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022", "2023", "2024", "2025"]
 
 
-                           # Don't Remove Credit @KILLERxVIPP
-                           # Subscribe YouTube Channel For Amazing Bot @KILLERxVIP
-                           # Ask Doubt on telegram @KILLERxVIPP
+                           # Don't Remove Credit @VJ_Botz
+                           # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+                           # Ask Doubt on telegram @KingVJ01
 
 
 # Online Stream and Download
